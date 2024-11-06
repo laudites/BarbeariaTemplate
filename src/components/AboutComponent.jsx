5c87cd5ace420b767da6c466e1f7bbaafe65a310
Willian Marques Laudites <w_laudites@hotmail.com>
2024-11-06 12:04:45 -0300

    2024_11_06_12:05

--- src/components/AboutComponent.jsx
+++ src/components/AboutComponent.jsx
@@ -4,49 +4,45 @@ import signature from "../img/gallery/signature.png";
 import aboutShape from "../img/gallery/about-shape.png";
 
 const AboutComponent = () => {
-
-    return(
-<>
-<section className="about-area section-padding30 position-relative">
-              <div className="container">
-                <div className="row align-items-center">
-                  <div className="col-lg-6 col-md-11">
-                    {/* about-img */}
-                    <div className="about-img ">
-                      <img src={about} alt="" />
-                    </div>
-                  </div>
-                  <div className="col-lg-6 col-md-12">
-                    <div className="about-caption">
-                      {/* Section Tittle */}
-                      <div className="section-tittle section-tittle3 mb-35">
-                        <span>About Our company</span>
-                        <h2>52 Years Of Experience In Hair cut!</h2>
-                      </div>
-                      <p className="mb-30 pera-bottom">
-                        Brook presents your services with flexible, convenient
-                        and cdpoe layouts. You can select your favorite layouts
-                        & elements for cular ts with unlimited ustomization
-                        possibilities. Pixel-perfreplication of the designers is
-                        intended.
-                      </p>
-                      <p className="pera-top mb-50">
-                        Brook presents your services with flexible, convefnient
-                        and ent anipurpose layouts. You can select your
-                        favorite.
-                      </p>
-                      <img src={signature} alt="" />
-                    </div>
-                  </div>
+  return (
+    <>
+      <section className="about-area section-padding30 position-relative">
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-lg-6 col-md-11">
+              {/* about-img */}
+              <div className="about-img">
+                <img src={about} alt="Imagem da barbearia" />
+              </div>
+            </div>
+            <div className="col-lg-6 col-md-12">
+              <div className="about-caption">
+                {/* Section Tittle */}
+                <div className="section-tittle section-tittle3 mb-35">
+                  <span>Sobre a nossa barbearia</span>
+                  <h2>Bem-vindo à NewEra Digital</h2>
                 </div>
+                <p className="mb-30 pera-bottom">
+                  A NewEra Digital abriu suas portas com uma proposta inovadora, inspirada nas clássicas barbearias, mas com um toque moderno. Hoje, somos uma das barbearias mais frequentadas de Goiânia, oferecendo um ambiente confortável e acolhedor para todos os nossos clientes.
+                </p>
+                <p className="pera-top mb-50">
+                  Nossos barbeiros são profissionais qualificados, cuidadosamente selecionados para atender tanto o público adulto quanto infantil, sempre com muita dedicação e cuidado para realçar o melhor visual para cada cliente.
+                </p>
+                <p className="pera-top mb-50">
+                  Na NewEra Digital, você encontra um ambiente familiar, estacionamento na porta, TVs com programação esportiva, música ambiente, preços acessíveis e, o mais importante, um atendimento de qualidade.
+                </p>
+                <img src={signature} alt="Assinatura" />
               </div>
-              {/* About Shape */}
-              <div className="about-shape">
-                <img src={aboutShape} alt="" />
-              </div>
-            </section>
-</>
-    )
+            </div>
+          </div>
+        </div>
+        {/* About Shape */}
+        <div className="about-shape">
+          <img src={aboutShape} alt="Forma decorativa" />
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default AboutComponent;
--- src/components/ServicesComponent.jsx
+++ src/components/ServicesComponent.jsx
@@ -1,95 +1,105 @@
 import React from 'react';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCut, faDumbbell, faClock } from "@fortawesome/free-solid-svg-icons";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import '../style/servicesComponent.css';
 
- const ServicesComponent = () => {
+// Dados dos serviços
+const servicesData = [
+  {
+    icon: "https://americanbarbershop.com.br/wp-content/uploads/2022/11/iconCorte.svg",
+    title: "Platinado",
+    description: "Utilizamos produtos da mais alta qualidade para deixar seus cabelos com um incrível loiro platinado.",
+  },
+  {
+    icon: "https://americanbarbershop.com.br/wp-content/uploads/2022/11/iconReconstrucao.svg",
+    title: "Reconstrução Capilar",
+    description: "Reconstrução dos cabelos, recuperando o seu brilho e fora natural.",
+  },
+  {
+    icon: "https://americanbarbershop.com.br/wp-content/uploads/2022/11/iconHidratacaoOzonio.svg",
+    title: "Hidratação com Ozônio",
+    description: "A hidratação capilar aliada ao ozônio promete muitos benefícios ao couro cabeludo e aos fios.",
+  },
+  {
+    icon: "https://americanbarbershop.com.br/wp-content/uploads/2022/11/iconPezinho.svg",
+    title: "Sobrancelha na Navalha",
+    description: "Apara e alinha os fios em excesso ao redor das sobrancelhas, fazendo uso de técnica com navalha.",
+  },
+  {
+    icon: "https://americanbarbershop.com.br/wp-content/uploads/2022/11/tesoura.svg",
+    title: "Corte de Cabelo",
+    description: "Corte de cabelo realizado com a excelência dos mestres barbeiros.",
+  },
+  {
+    icon: "https://americanbarbershop.com.br/wp-content/uploads/2022/11/iconBarba.svg",
+    title: "Barba",
+    description: "Barba feita como antigamente, com o uso de uma relaxante toalha quente e creme hidratante para amaciar os fios.",
+  },
+  {
+    icon: "https://americanbarbershop.com.br/wp-content/uploads/2022/11/iconPezinho.svg",
+    title: "Pezinho",
+    description: "Indicado para você que precisa fazer apenas a manutenção básica do contorno do cabelo.",
+  },
+  {
+    icon: "https://americanbarbershop.com.br/wp-content/uploads/2022/11/iconSelagem.svg",
+    title: "Selagem",
+    description: "Tratamento capilar feito com produtos de alta qualidade. Alisa os cabelos e sela os fios.",
+  },
+  {
+    icon: "https://americanbarbershop.com.br/wp-content/uploads/2022/11/barboterapiaIcone.svg",
+    title: "Barboterapia com Ozônio",
+    description: "Barba feita com o uso de uma relaxante toalha quente e creme hidratante para amaciar os fios.",
+  },
+  {
+    icon: "https://americanbarbershop.com.br/wp-content/uploads/2022/11/iconHidratacao.svg",
+    title: "Hidratação Capilar",
+    description: "Tratamento ideal para manter os cabelos saudáveis. Restaura o brilho natural, os nutrientes e a queratina perdida.",
+  }
+];
 
-    return(
-<>
-<section className="service-area pb-170">
-              <div className="container">
-                {/* Section Tittle */}
-                <div className="row d-flex justify-content-center">
-                  <div className="col-xl-7 col-lg-8 col-md-11 col-sm-11">
-                    <div className="section-tittle text-center mb-90">
-                      <span>Professional Services</span>
-                      <h2>Our Best services that we offering to you</h2>
-                    </div>
-                  </div>
-                </div>
-                {/* Section caption */}
-                <div className="row">
-                  <div className="col-xl-4 col-lg-4 col-md-6">
-                    <div className="services-caption text-center mb-30">
-                      <div className="serviceGeral">
-                        <div className="service-icon">
-                          <FontAwesomeIcon
-                            icon={faCut}
-                            size="2x"
-                            color="white"
-                          />
-                        </div>
-                        <div className="service-cap">
-                          <h4>
-                            <a href="#">Stylish Hair Cut</a>
-                          </h4>
-                          <p>
-                            Sorem spsum dolor sit amsectetur adipisclit, seddo
-                            eiusmod tempor incididunt ut laborea.
-                          </p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-xl-4 col-lg-4 col-md-6">
-                    <div className="services-caption active text-center mb-30">
-                      <div className="serviceGeral">
-                        <div className="service-icon">
-                          <FontAwesomeIcon
-                            icon={faDumbbell}
-                            size="2x"
-                            color="white"
-                          />
-                        </div>
-                        <div className="service-cap">
-                          <h4>
-                            <a href="#">Body Massage</a>
-                          </h4>
-                          <p>
-                            Sorem spsum dolor sit amsectetur adipisclit, seddo
-                            eiusmod tempor incididunt ut laborea.
-                          </p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-xl-4 col-lg-4 col-md-6">
-                    <div className="services-caption text-center mb-30">
-                      <div className="serviceGeral">
-                        <div className="service-icon">
-                          <FontAwesomeIcon
-                            icon={faClock}
-                            size="2x"
-                            color="white"
-                          />
-                        </div>
-                        <div className="service-cap">
-                          <h4>
-                            <a href="#">Beard Style</a>
-                          </h4>
-                          <p>
-                            Sorem spsum dolor sit amsectetur adipisclit, seddo
-                            eiusmod tempor incididunt ut laborea.
-                          </p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+export default function App() {
+  return (
+    <>
+      <Swiper
+        spaceBetween={30}  // Espaço entre os slides
+        slidesPerView={3}  // Número de slides visíveis
+        loop={true}  // Habilita o loop infinito
+        autoplay={{
+          delay: 2500,  // Tempo de espera entre os slides
+          disableOnInteraction: false,  // Não desabilita ao interagir
+        }}
+        breakpoints={{
+          640: {
+            slidesPerView: 1,  // Um slide visível em telas pequenas
+          },
+          768: {
+            slidesPerView: 2,  // Dois slides visíveis em telas médias
+          },
+          1024: {
+            slidesPerView: 3,  // Três slides visíveis em telas grandes
+          },
+        }}
+        navigation={true}  // Habilita a navegação
+        className="mySwiper"
+      >
+        {servicesData.map((service, index) => (
+          <SwiperSlide key={index}>
+            <div className="serviceGeral services-caption text-center mb-30 per001">
+              <div className="service-icon">
+                <img src={service.icon} alt={service.title} width="50" height="50" />
               </div>
-            </section>
-</>
-    )
-};
-
-export default ServicesComponent;
+              <div className="service-cap">
+                <h4>{service.title}</h4>
+                <p>{service.description}</p>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
+  );
+}
